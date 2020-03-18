1c9a20d91cf3aabfc1f3fc44f29618975f7fca10
Leslie Chung <hklchung@gmail.com>
2020-03-18 16:57:49 +1100

    Updated EEPROm code to take values between 0~255

--- EEPROM_programming.ino
+++ EEPROM_programming.ino
@@ -1,35 +1,75 @@
 #include <EEPROM.h>
-int address;
-int data;
+String input="";
 
-// the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  String request = "";
-  if (Serial.available()){
-    request = Serial.readString();
-    if (request.startsWith("read")){
-      address = request.substring(request.indexOf(' ') +1).toInt();
-      Serial.print("Reading data from address ");
-      Serial.print(address);
-      Serial.print(": ");
-      Serial.print(" ");
-      Serial.println(EEPROM.read(address));
-      Serial.println();
-    } else if (request.startsWith("write")) {
-    address = request.substring(6,7).toInt();
-    data = request.substring(8).toInt();
-    Serial.print("Writing ");
-    Serial.print(data);
-    Serial.print(" to the following address: ");
-    Serial.print(address);
-    EEPROM.write(address,data);
-    Serial.println();
-    }
-  }
+if (Serial.available()) {
+input = Serial.readString();
+String request = "";
+int readmode = -1;
+int address = -1;
+int data = -1;
+int count = 1;
+const char* Input = input.c_str();
+for (int i = 0;i<strlen(Input);i++) {
+char x = input[i];
+if (x == ' ') {
+if (count == 1) {
+if (request == "write") {
+readmode = 0;
+}
+else if (request == "read") {
+readmode = 1;
+}
+count = 2;
+}
+else if (count == 2) {
+const char* addr = request.c_str();
+sscanf(addr,"%d",&address);
+count = 3;
+}
+else if (count == 3) {
+break;
+}
+request = "";
+}
+else {
+request = request + x;
+}
+}
+
+if (readmode == 0) {
+const char* dat = request.c_str();
+sscanf(dat,"%d",&data);
+if (address>=0 && address<512 && data>=0 && data<256) {
+EEPROM.write(address,data);
+Serial.print("Writing to address: ");
+Serial.println(address);
+Serial.print("Value saved: ");
+Serial.println(data);
+}
+else {
+Serial.println("Please try again");
+}
+}
+else if (readmode == 1) {
+if (count == 2) {
+const char* addr = request.c_str();
+sscanf(addr,"%d",&address);
+}
+if (address>=0 && address<512) {
+data = EEPROM.read(address);
+Serial.print("Reading from address: ");
+Serial.println(address);
+Serial.print("Value returned: ");
+Serial.println(data);
+}
+else {
+Serial.println("Please try again");
+}
+}
+}
 }
